38dc6b7b8e9f5013df6945733d242a672e997797
frdrk00 <masterkooo@gmail.com>
2023-07-17 23:17:26 +0200

    Setup

--- schemas/dish.js
+++ schemas/dish.js
@@ -1,15 +1,26 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'category',
-  title: 'Menu Category',
+  name: 'dish',
+  title: 'Dish',
   type: 'document',
   fields: [
     defineField({
       name: 'name',
       type: 'string',
-      title: 'Category name',
+      title: 'Name of dish',
       validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'short_description',
+      type: 'string',
+      title: 'Short description',
+      validation: (Rule) => Rule.max(200),
+    }),
+    defineField({
+      name: 'price',
+      type: 'number',
+      title: 'Price of the dish in EUR',
     }),
     defineField({
       name: 'image',
